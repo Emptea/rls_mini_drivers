2bfb8b88874b933419bf8343ba9a09b2e68f57b7
Emptea <aryas1725@gmail.com>
2026-05-14 17:27:11 +0300

    upd axi multiplier & test

--- axi_multiplier/usertest/lib/axi_multiplier.c
+++ axi_multiplier/usertest/lib/axi_multiplier.c
@@ -4,6 +4,16 @@
 #include <unistd.h>
 #include "axi_multiplier.h"
 
+enum regmap
+{
+    REGMAP_IP_VER = 0x00,
+    REGMAP_KILL = 0x04,
+    REGMAP_TP = 0x08,
+    REGMAP_CH = 0x0C,
+    REGMAP_MULT0 = 0x10,
+    REGMAP_MULT1 = 0x14,
+};
+
 int fd;
 
 uint32_t axi_multiplier_init()
@@ -22,11 +32,10 @@ void axi_multiplier_deinit()
     close(fd);
 }
 
-uint32_t axi_multiplier_write(int16_t val)
+uint32_t axi_multiplier_write(uint32_t val, uint32_t regmap_offset)
 {
-    uint32_t val_u32 = (uint32_t)(int32_t)val;
-
-    if (write(fd, &val_u32, sizeof(val_u32)) != sizeof(val_u32))
+    lseek(fd, regmap_offset, SEEK_SET);
+    if (write(fd, &val, sizeof(val)) != sizeof(val))
     {
         perror("write");
         axi_multiplier_deinit();
@@ -36,10 +45,11 @@ uint32_t axi_multiplier_write(int16_t val)
     return FD_ERR_NONE;
 }
 
-uint32_t axi_multiplier_read(int16_t *val)
+uint32_t axi_multiplier_read(uint32_t *val, uint32_t regmap_offset)
 {
     uint32_t rd;
-    
+
+    lseek(fd, regmap_offset, SEEK_SET);
     if (read(fd, &rd, sizeof(rd)) != sizeof(rd))
     {
         perror("read");
@@ -47,6 +57,79 @@ uint32_t axi_multiplier_read(int16_t *val)
         return FD_ERR_READ;
     }
 
-    *val = (int16_t)rd;
+    *val = rd;
     return FD_ERR_NONE;
+}
+
+void axi_multiplier_get_ip_ver(uint16_t *ver_maj, uint16_t *ver_min)
+{
+    uint32_t ver;
+    axi_multiplier_read(&ver, REGMAP_IP_VER);
+    *ver_maj = (uint16_t)(0xFFFF & (ver >> 16));
+    *ver_min = (uint16_t)(0xFFFF & ver);
+}
+
+uint32_t axi_multiplier_get_kill()
+{
+    uint32_t kill;
+    axi_multiplier_read(&kill, REGMAP_KILL);
+    return kill;
+}
+
+uint32_t axi_multiplier_get_tp()
+{
+    uint32_t tp;
+    axi_multiplier_read(&tp, REGMAP_TP);
+    return tp;
+}
+
+uint32_t axi_multiplier_get_ch()
+{
+    uint32_t ch;
+    axi_multiplier_read(&ch, REGMAP_CH);
+    return ch;
+}
+
+int16_t axi_multiplier_get_mult(uint32_t ch_num)
+{
+    uint32_t data, regmap_offset;
+    int16_t mult_val;
+    regmap_offset = REGMAP_MULT0 + 4 * ch_num;
+    axi_multiplier_read(&data, regmap_offset);
+    mult_val = (int16_t)data;
+    return mult_val;
+}
+
+void axi_multiplier_set_kill()
+{
+    axi_multiplier_write(1, REGMAP_KILL);
+}
+
+void axi_multiplier_reset_kill()
+{
+    axi_multiplier_write(0, REGMAP_KILL);
+}
+
+void axi_multiplier_reset()
+{
+    axi_multiplier_set_kill();
+    axi_multiplier_reset_kill();
+}
+
+void axi_multiplier_set_tp(uint32_t tp)
+{
+    axi_multiplier_write(tp, REGMAP_TP);
+}
+
+void axi_multiplier_set_ch(uint32_t ch)
+{
+    axi_multiplier_write(ch, REGMAP_CH);
+}
+
+void axi_multiplier_set_mult(int16_t val, uint32_t ch_num)
+{
+    uint32_t val_u32 = (uint32_t)(int32_t)val;
+    uint32_t regmap_offset;
+    regmap_offset = REGMAP_MULT0 + 4 * ch_num;
+    axi_multiplier_write(val_u32, regmap_offset);
 }
--- axi_multiplier/usertest/test_axi_multiplier.c
+++ axi_multiplier/usertest/test_axi_multiplier.c
@@ -3,9 +3,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define RESET "\033[0m"
+#define RED "\033[31m" /* Red */
+
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("usage: %s <value>\n", argv[0]);
         return 1;
     }
@@ -13,26 +17,50 @@ int main(int argc, char *argv[])
     int16_t wr = (int16_t)strtol(argv[1], NULL, 0);
     int16_t rd;
 
-    if (axi_multiplier_init()) {
+    if (axi_multiplier_init())
+    {
         return 1;
     }
+    uint16_t ver_maj, ver_min;
+    axi_multiplier_get_ip_ver(&ver_maj, &ver_min);
+    printf("AXI Multiplier v%d.%d\n", ver_maj, ver_min);
 
-    if (axi_multiplier_write(wr)) {
-        axi_multiplier_deinit();
-        return 1;
+    uint32_t tp = 2;
+    uint32_t tp_get;
+    axi_multiplier_set_tp(tp);
+    tp_get = axi_multiplier_get_tp();
+    printf("Set test point %d, returned %d", tp, tp_get);
+    if (tp != tp_get)
+    {
+        printf(RED "Test point check failed" RESET);
     }
-    printf("axi_multiplier: wrote 0x%04hx\n", (uint16_t)wr);
 
-    if (axi_multiplier_read(&rd)) {
-        axi_multiplier_deinit();
-        return 1;
+    uint32_t ch = 1;
+    uint32_t ch_get;
+    axi_multiplier_set_tp(ch);
+    ch_get = axi_multiplier_get_ch();
+    printf("Set channel %d, returned %d", ch, ch_get);
+    if (ch != ch_get)
+    {
+        printf(RED "Channel check failed" RESET);
     }
-    printf("axi_multiplier: read 0x%04hx\n", (uint16_t)rd);
 
-    if (wr != rd) {
-        printf("wr != rd\n");
-        axi_multiplier_deinit();
-        return 1;
+    int16_t mult0 = 2;
+    int16_t mult1 = -2;
+    int16_t mult0_get, mult1_get;
+    axi_multiplier_set_mult(mult0, 0);
+    mult0_get = axi_multiplier_get_mult(0);
+    axi_multiplier_set_mult(mult1, 1);
+    mult1_get = axi_multiplier_get_mult(1);
+    printf("Set multiplier value %d for ch %d, returned %d", mult0, 0, mult0_get);
+    if (mult0 != mult0_get)
+    {
+        printf(RED "Value is not set into multiplier ch 0" RESET);
+    }
+    printf("Set multiplier value %d for ch %d, returned %d", mult1, 1, mult1_get);
+    if (mult1 != mult1_get)
+    {
+        printf(RED "Value is not set into multiplier ch 1" RESET);
     }
 
     axi_multiplier_deinit();

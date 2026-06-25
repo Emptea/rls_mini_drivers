99f0769e92721e0c544cef23592510951d72532d
Emptea <aryas1725@gmail.com>
2026-06-25 11:43:15 +0000

    sends and receives once

--- ip_comm_test/src/ip_comm_test.cpp
+++ ip_comm_test/src/ip_comm_test.cpp
@@ -1,5 +1,6 @@
 #include "axi_dsp.h"
 #include "dma_channel.hpp"
+#include "misc.h"
 
 #include <cstdint>
 #include <fcntl.h>
@@ -8,6 +9,7 @@
 #include <piliterals_time.h>
 #include <piscreen.h>
 #include <pisignals.h>
+#include <pistring_std.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -63,23 +65,31 @@ int main(int argc, char * argv[]) {
 	axi_dsp_apply();
 
 	int buf_size             = BUFFER_SIZE;
-	uint32_t num_transfers   = 0;
+	uint32_t num_transfers   = 1;
 	uint32_t n_samps_per_buf = 232;
 
-	PIVector<dma_channel> dma_channels;
+	PIVector<dma_channel *> dma_channels;
+
 	void * rx_buffers[RX_BUFFER_COUNT];
 	void * tx_buffers[NUM_CHANNELS_TX][TX_BUFFER_COUNT];
 
 	dma_channels.resize(NUM_CHANNELS_TX + NUM_CHANNELS_RX);
+	for (int i = 0; i < NUM_CHANNELS_TX + NUM_CHANNELS_RX; i++) {
+		dma_channels[i] = new dma_channel();
+	}
 
 	PIString tx_devnodes[NUM_CHANNELS_TX] =
 		{TX_DEV_CH0, TX_DEV_CH1, TX_DEV_CH2, TX_DEV_CH3, TX_DEV_CH4, TX_DEV_CH5, TX_DEV_CH6, TX_DEV_CH7};
 
-	ch_config rx_config = {.devnode = RX_DEV, .buffer_size = BUFFER_SIZE, .buffer_count = RX_BUFFER_COUNT};
-	ch_config tx_config = {.buffer_size = BUFFER_SIZE, .buffer_count = TX_BUFFER_COUNT};
-	dma_channels[0].init(rx_config);
-	dma_channels[0].set_save_to_file(output_file, n_samps_per_buf) dma_channels[0].set_num_transfers(num_transfers);
-	dma_channels[0].get_all_buffers(rx_buffers);
+	dma_channel::ch_config rx_config = {.devnode = RX_DEV, .buffer_size = BUFFER_SIZE, .buffer_count = RX_BUFFER_COUNT};
+	dma_channel::ch_config tx_config = {.buffer_size = BUFFER_SIZE, .buffer_count = TX_BUFFER_COUNT};
+	piCout << "Wait for DMA init";
+	dma_channels[0]->init(rx_config);
+	dma_channels[0]->set_save_to_file(output_file, n_samps_per_buf);
+	dma_channels[0]->set_num_transfers(num_transfers * N_PACKS_IN_TX_BUF);
+	for (size_t i = 0; i < RX_BUFFER_COUNT; i++) {
+		rx_buffers[i] = dma_channels[0]->get_buffer(i);
+	}
 	piCout << "Rx buffers adresses are:";
 	for (size_t i = 0; i < RX_BUFFER_COUNT; i++) {
 		piCout << "num " << i << " " << PICoutManipulators::PICoutFormat::Hex << rx_buffers[i];
@@ -87,31 +97,38 @@ int main(int argc, char * argv[]) {
 
 	piCout << "Tx buffer adresses are:";
 	for (size_t i = 0; i < NUM_CHANNELS_TX; i++) {
-		tx_config.devnode = tx_devnodes[i] dma_channels[i + 1].init(tx_config);
-		dma_channels[i + 1].set_num_transfers(num_transfers);
-		dma_channels[i + 1].get_all_buffers(tx_buffers[i]);
-		for (size_t k; k < TX_BUFFER_COUNT; k++) {
-			piCout << "ch" << k << " " << PICoutManipulators::PICoutFormat::Hex << tx_buffers[i][k];
+		tx_config.devnode = PIString2StdString(tx_devnodes[i]);
+		dma_channels[i + 1]->init(tx_config);
+		dma_channels[i + 1]->set_num_transfers(num_transfers);
+		dma_channels[i + 1]->get_all_buffers(tx_buffers[i]);
+		for (size_t k = 0; k < TX_BUFFER_COUNT; k++) {
+			piCout << "ch" << i << " buf" << k << " " << PICoutManipulators::PICoutFormat::Hex << tx_buffers[i][k];
 		}
 	}
 
 	int buff_id = 0;
 	for (size_t i = 0; i < num_transfers; i++) {
 		uint8_t * current_buffers[TX_BUFFER_COUNT];
-		for (size_t k = 0; k < TX_BUFFER_COUNT; k++) {
-			current_buffers[k] = tx_buffers[buff_id][k]
+		for (size_t k = 0; k < NUM_CHANNELS_TX; k++) {
+			current_buffers[k] = (uint8_t *)tx_buffers[k][buff_id];
 		}
-		misc_read_8chs_from_file(input_file, tx_buffers, buf_size);
-		for (size_t k = TX_BUFFER_COUNT + RX_BUFFER_COUNT - 1; k >= 0; k--) {
-			dma_channels[k].single_transfer();
+		misc_read_8chs_from_file(input_file, current_buffers, buf_size);
+		for (int k = dma_channels.size() - 1; k >= 0; k--) {
+			dma_channels[k]->start_transfer();
+		}
+		10_ms .sleep();
+		for (int k = dma_channels.size() - 1; k >= 0; k--) {
+			dma_channels[k]->wait_for_transfer();
 		}
 		buff_id = (buff_id + 1) % TX_BUFFER_COUNT;
 	}
 
 	WAIT_FOR_EXIT;
 
-	for (size_t k = TX_BUFFER_COUNT + RX_BUFFER_COUNT - 1; k >= 0; k--) {
-		dma_channels[k].cleanup();
+	for (int k = dma_channels.size() - 1; k >= 0; k--) {
+		dma_channels[k]->cleanup();
+		delete dma_channels[k];
+		dma_channels[k] = nullptr;
 	}
 	axi_dsp_deinit();
 	piDeleteSafety(kbd);

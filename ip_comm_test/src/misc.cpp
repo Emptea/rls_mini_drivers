67fa993189399627dc1feefc9d7b54a1db9b1cf4
ki4igin <ki4igin1992@outlook.com>
2026-09-23 10:27:01 +0300

    clang-format

--- ip_comm_test/src/misc.cpp
+++ ip_comm_test/src/misc.cpp
@@ -1,16 +1,15 @@
 #include "misc.h"
-#include <chrono>
-#include <iomanip>
-
-#include <unistd.h>
-#include <sys/select.h>
-#include <cstdio>
-#include <errno.h>
-#include <string.h>
 
 #include <atomic>
-#include <thread>
+#include <chrono>
+#include <cstdio>
+#include <errno.h>
+#include <iomanip>
 #include <iostream>
+#include <string.h>
+#include <sys/select.h>
+#include <thread>
+#include <unistd.h>
 
 
 /**
@@ -125,94 +124,85 @@ int misc_read_8chs_from_file(const char * filename, uint8_t * buffers[8], size_t
 	return 0;
 }
 
-std::string misc_get_date()
-{
-    time_t now = time(nullptr);
-    tm *local_time = localtime(&now);
+std::string misc_get_date() {
+	time_t now      = time(nullptr);
+	tm * local_time = localtime(&now);
 
-    std::ostringstream oss;
-    oss << std::put_time(local_time, "%Y-%m-%d");
-    return oss.str();
+	std::ostringstream oss;
+	oss << std::put_time(local_time, "%Y-%m-%d");
+	return oss.str();
 }
 
-std::string misc_get_datetime()
-{
-    time_t now = time(nullptr);
-    tm *local_time = localtime(&now);
+std::string misc_get_datetime() {
+	time_t now      = time(nullptr);
+	tm * local_time = localtime(&now);
 
-    std::ostringstream oss;
-    oss << std::put_time(local_time, "%Y-%m-%d_%H-%M-%S");
-    return oss.str();
+	std::ostringstream oss;
+	oss << std::put_time(local_time, "%Y-%m-%d_%H-%M-%S");
+	return oss.str();
 }
 
-std::string misc_get_timestamp_ms()
-{
-    using namespace std::chrono;
-    static high_resolution_clock::time_point start_time = high_resolution_clock::now();
-    high_resolution_clock::time_point now = high_resolution_clock::now();
-    duration<long long, std::milli> diff = duration_cast<milliseconds>(now - start_time);
-    return std::to_string(diff.count());
+std::string misc_get_timestamp_ms() {
+	using namespace std::chrono;
+	static high_resolution_clock::time_point start_time = high_resolution_clock::now();
+	high_resolution_clock::time_point now               = high_resolution_clock::now();
+	duration<long long, std::milli> diff                = duration_cast<milliseconds>(now - start_time);
+	return std::to_string(diff.count());
 }
 
-bool misc_stdin_has_data()
-{
-    fd_set set;
-    FD_ZERO(&set);
-    FD_SET(STDIN_FILENO, &set);
+bool misc_stdin_has_data() {
+	fd_set set;
+	FD_ZERO(&set);
+	FD_SET(STDIN_FILENO, &set);
 
-    timeval timeout{};
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 0; // do not wait
+	timeval timeout{};
+	timeout.tv_sec  = 0;
+	timeout.tv_usec = 0; // do not wait
 
-    return select(STDIN_FILENO + 1, &set, nullptr, nullptr, &timeout) > 0;
+	return select(STDIN_FILENO + 1, &set, nullptr, nullptr, &timeout) > 0;
 }
 
 std::atomic<bool> has_input(false);
 
-static volatile void input_tread_work()
-{
-    std::string input;
-    while (true) {
-        getline(std::cin, input);
-        has_input = true;
-    }
+static volatile void input_tread_work() {
+	std::string input;
+	while (true) {
+		getline(std::cin, input);
+		has_input = true;
+	}
 }
 
-void misc_init_input_tread()
-{
-    std::thread t(input_tread_work);
-    t.detach();
+void misc_init_input_tread() {
+	std::thread t(input_tread_work);
+	t.detach();
 }
 
-bool misc_has_input()
-{
-    return has_input;
+bool misc_has_input() {
+	return has_input;
 }
 
-void misc_to_binstr(const int a[], int len, char result[])
-{
-    for (int i = 0; i < len; i++) {
-        result[len - 1 - i] = a[i] + '0'; // Store with MSB first
-    }
-    result[len] = '\0';
+void misc_to_binstr(const int a[], int len, char result[]) {
+	for (int i = 0; i < len; i++) {
+		result[len - 1 - i] = a[i] + '0'; // Store with MSB first
+	}
+	result[len] = '\0';
 }
 
-bool misc_arr_bin2uint16(const uint bits[], int len, uint16_t result[], int res_len)
-{
-    if (res_len * 16 < len) {
-        return false;
-    }
+bool misc_arr_bin2uint16(const uint bits[], int len, uint16_t result[], int res_len) {
+	if (res_len * 16 < len) {
+		return false;
+	}
 
-    for (int i = 0; i < res_len; i++) {
-        result[i] = 0;
-    }
+	for (int i = 0; i < res_len; i++) {
+		result[i] = 0;
+	}
 
-    for (int i = 0; i < len; i++) {
-        if (bits[i]) {
-            int word_index = i / 16;
-            int bit_position = i % 16;
-            result[word_index] |= (bits[i] << bit_position);
-        }
-    }
-    return true;
+	for (int i = 0; i < len; i++) {
+		if (bits[i]) {
+			int word_index   = i / 16;
+			int bit_position = i % 16;
+			result[word_index] |= (bits[i] << bit_position);
+		}
+	}
+	return true;
 }
